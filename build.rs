625c49f8a0a97209baa8444cb88f0ee9f066e51c
Alexander Avramenko <a.avramenko@kryptonite.ru>
2021-04-11 13:53:19 +0300

    Remove obsolete local proto & fix build

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/migchat.proto")?;
+    tonic_build::compile_protos("third-party/migchat-proto/migchat.proto")?;
     Ok(())
 }
